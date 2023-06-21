09eb80f31325e2e65302b99929d6d4cc8301f4b2
Ahamed Arshad <arshad987.mla@gmail.com>
2023-06-21 09:57:47 +0530

    Sucess

--- Insert_Queries.sql
+++ Insert_Queries.sql
@@ -1,3 +1,6 @@
 USE Agrarian;
-Insert into User_Data(Username,User_Type,Email,Password,Phone_Number) values("Jhon", "Farmer", "Jhon@gmail.com", "Jhon.12345", 0771234567);
-Select * from User_Data;
+Insert into User_Data(Username,User_Type,Email,Password,Phone_Number) 
+values("Jhon", "Farmer", "Jhon@gmail.com", "Jhon.12345", 0771234567),
+("Chris", "Mentor", "Cris@gmail.com", "Cris.12345", 0772236587),
+("Thomas", "Farmer", "Thomas@gmail.com", "Thomas@1234", 0772236469),
+("Angelina", "Mentor", "Angelina@gmail.com", "Angelina.64_45", 0771434855);
